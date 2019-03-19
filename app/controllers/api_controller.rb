b2193114dd0549591e7b0f30eca5576137e53dcc
Daniel Hopler <hopler.daniel@gmail.com>
2019-03-19 15:19:33 -0400

    updated authenticated

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,12 +1,10 @@
 class ApiController < ApplicationController
 	skip_before_action :verify_authenticity_token
 
-	respond_to :json
-
 
 	private
 
 	def authenticated?
-		authenticate_with_http_basic{|username, password| User.where( username: username, password: password).present? }
+		authenticate_or_request_with_http_basic{|username, password| User.where( username: username, password_digest: password).present? }
 	end
 end
